72801aad072598db2420d0c37a474357632e4e47
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2015-01-28 15:48:19 +0000

    Fix all OSGB tests.

--- lib/iris/tests/unit/analysis/cartography/test_change_vector_basis.py
+++ lib/iris/tests/unit/analysis/cartography/test_change_vector_basis.py
@@ -31,12 +31,14 @@ from iris.coords import DimCoord, AuxCoord
 import iris.coord_systems
 
 
-def uv_cubes(shape=(5, 6)):
+def uv_cubes(shape=(5, 6), x=None, y=None):
     """Return u, v cubes with a grid in a rotated pole CRS."""
     cs = iris.coord_systems.RotatedGeogCS(grid_north_pole_latitude=37.5,
                                           grid_north_pole_longitude=177.5)
-    x = np.linspace(311.9, 391.1, shape[1])
-    y = np.linspace(-23.6, 24.8, shape[0])
+    if x is None:
+        x = np.linspace(311.9, 391.1, shape[1])
+    if y is None:
+        y = np.linspace(-23.6, 24.8, shape[0])
     x2d, y2d = np.meshgrid(x, y)
     u = 10 * (2 * np.cos(2 * np.deg2rad(x2d) + 3 * np.deg2rad(y2d + 30)) ** 2)
     v = 20 * np.cos(6 * np.deg2rad(x2d))
@@ -52,13 +54,16 @@ def uv_cubes(shape=(5, 6)):
     return u_cube, v_cube
 
 
-def uv_cubes_3d(shape=(3, 5, 6)):
-    """Return 3d u, v cubes with a grid in a rotated pole CRS."""
-    cs = iris.coord_systems.RotatedGeogCS(grid_north_pole_latitude=37.5,
-                                          grid_north_pole_longitude=177.5)
-    n_realization = shape[0]
-    x = np.linspace(311.9, 391.1, shape[2])
-    y = np.linspace(-23.6, 24.8, shape[1])
+def uv_cubes_3d(ref_cube, n_realization=3):
+    """
+    Return 3d u, v cubes with a grid in a rotated pole CRS.
+
+    Based on the given 2d cube, with added leading dim * 'n_realization'.
+
+    """
+    cs = ref_cube.coord(axis='x').coord_system
+    x = ref_cube.coord(axis='x').points
+    y = ref_cube.coord(axis='y').points
     x2d, y2d = np.meshgrid(x, y)
     u = 10 * (2 * np.cos(2 * np.deg2rad(x2d) + 3 * np.deg2rad(y2d + 30)) ** 2)
     v = 20 * np.cos(6 * np.deg2rad(x2d))
@@ -127,14 +132,11 @@ class TestPrerequisites(tests.IrisTest):
             change_vector_basis(u, v, iris.coord_systems.OSGB())
 
 
-class TestAnalyticalComparison(tests.IrisTest):
-    def setUp(self):
-        self.test_uv = uv_cubes()
-        self.test_cs = iris.coord_systems.GeogCS(1.0)
-
+class TestAnalyticComparison(tests.IrisTest):
     def test_rotated_to_true(self):
-        u_rot, v_rot = self.test_uv
-        u_true, v_true = change_vector_basis(u_rot, v_rot, self.test_cs)
+        u_rot, v_rot = uv_cubes()
+        test_cs = iris.coord_systems.GeogCS(1.0)
+        u_true, v_true = change_vector_basis(u_rot, v_rot, test_cs)
 
         # Calculate "exact" results to compare.
         # (equations for rotated-pole transformation : cf. UMDP015)
@@ -176,8 +178,21 @@ class TestAnalyticalComparison(tests.IrisTest):
 
 
 class TestRotatedToOSGB(tests.IrisTest):
+    # Define some coordinate ranges for the uv_cubes 'standard' RotatedPole
+    # system, that exceed the OSBG margins, but not by "too much".
+    _rp_x_min, _rp_x_max = -5.0, 5.0
+    _rp_y_min, _rp_y_max = -5.0, 15.0
+
+    def _uv_cubes_OSGB(self, shape=(5, 6)):
+        # Make test cubes suitable for transforming to OSGB, as the standard
+        # 'uv_cubes' result goes too far outside, leading to errors.
+        ny, nx = shape
+        x = np.linspace(self._rp_x_min, self._rp_x_max, nx)
+        y = np.linspace(self._rp_y_min, self._rp_y_max, ny)
+        return uv_cubes(x=x, y=y, shape=shape)
+
     def test_name(self):
-        u, v = uv_cubes()
+        u, v = self._uv_cubes_OSGB()
         u.rename('bob')
         v.rename('alice')
         ut, vt = change_vector_basis(u, v, iris.coord_systems.OSGB())
@@ -187,11 +202,11 @@ class TestRotatedToOSGB(tests.IrisTest):
     def test_new_coords(self):
         nx = 6
         ny = 5
-        u, v = uv_cubes((ny, nx))
+        u, v = self._uv_cubes_OSGB((ny, nx))
         ut, vt = change_vector_basis(u, v, iris.coord_systems.OSGB())
         # x, y values taken from uv_cubes().
-        x = np.linspace(311.9, 391.1, nx)
-        y = np.linspace(-23.6, 24.8, ny)
+        x = np.linspace(self._rp_x_min, self._rp_x_max, nx)
+        y = np.linspace(self._rp_y_min, self._rp_y_max, ny)
         x2d, y2d = np.meshgrid(x, y)
         src_crs = ccrs.RotatedPole(pole_longitude=177.5, pole_latitude=37.5)
         tgt_crs = ccrs.OSGB()
@@ -214,7 +229,7 @@ class TestRotatedToOSGB(tests.IrisTest):
         self.assertEqual(vt.coord('projection_y_coordinate'), expected_y)
 
     def test_orig_coords(self):
-        u, v = uv_cubes()
+        u, v = self._uv_cubes_OSGB()
         ut, vt = change_vector_basis(u, v, iris.coord_systems.OSGB())
         self.assertEqual(u.coord('grid_latitude'), ut.coord('grid_latitude'))
         self.assertEqual(v.coord('grid_latitude'), vt.coord('grid_latitude'))
@@ -222,39 +237,42 @@ class TestRotatedToOSGB(tests.IrisTest):
         self.assertEqual(v.coord('grid_longitude'), vt.coord('grid_longitude'))
 
     def test_magnitude_preservation(self):
-        u, v = uv_cubes()
+        u, v = self._uv_cubes_OSGB()
         ut, vt = change_vector_basis(u, v, iris.coord_systems.OSGB())
         orig_sq_mag = u.data**2 + v.data**2
         res_sq_mag = ut.data**2 + vt.data**2
-        self.assertArrayAlmostEqual(orig_sq_mag, res_sq_mag)
+        self.assertArrayAllClose(orig_sq_mag, res_sq_mag, rtol=5e-4)
 
     def test_data_values(self):
+        u, v = self._uv_cubes_OSGB()
         # Slice out 4 points that lie in and outside OSGB extent.
-        u, v = uv_cubes()
-        u = u[1:3, 2:4]
-        v = v[1:3, 2:4]
+        u = u[1:3, 3:5]
+        v = v[1:3, 3:5]
         ut, vt = change_vector_basis(u, v, iris.coord_systems.OSGB())
-        # Values precalculated using cartopy.transform_vectors().
-        expected_ut_data = np.array([[17.15565333, 6.77094137],
-                                     [5.28747914, 0.14330255]])
-        expected_vt_data = np.array([[-2.12938771, 19.97203449],
-                                     [-3.01654362, 19.96260676]])
+        # Values precalculated and checked.
+        expected_ut_data = np.array([[0.16285514,  0.35323639],
+                                     [1.82650698,  2.62455840]])
+        expected_vt_data = np.array([[19.88979966,  19.01921346],
+                                     [19.88018847,  19.01424281]])
         # Compare u and v data values against previously calculated values.
-        self.assertArrayAlmostEqual(ut.data, expected_ut_data)
-        self.assertArrayAlmostEqual(vt.data, expected_vt_data)
+        self.assertArrayAllClose(ut.data, expected_ut_data, rtol=1e-5)
+        self.assertArrayAllClose(vt.data, expected_vt_data, rtol=1e-5)
 
     def test_nd_data(self):
-        u, v = uv_cubes_3d()
-        u = u[:, 1:3, 2:4]
-        v = v[:, 1:3, 2:4]
+        u2d, y2d = self._uv_cubes_OSGB()
+        u, v = uv_cubes_3d(u2d)
+        u = u[:, 1:3, 3:5]
+        v = v[:, 1:3, 3:5]
         ut, vt = change_vector_basis(u, v, iris.coord_systems.OSGB())
-        # Values precalculated using cartopy.transform_vectors() and
+        # Values precalculated and checked (as test_data_values above),
         # then scaled by factor [1, 2, 3] along 0th dim (see uv_cubes_3d()).
+        expected_ut_data = np.array([[0.16285514,  0.35323639],
+                                     [1.82650698,  2.62455840]])
+        expected_vt_data = np.array([[19.88979966,  19.01921346],
+                                     [19.88018847,  19.01424281]])
         factor = np.array([1, 2, 3]).reshape(3, 1, 1)
-        expected_ut_data = factor * np.array([[17.15565333, 6.77094137],
-                                              [5.28747914, 0.14330255]])
-        expected_vt_data = factor * np.array([[-2.12938771, 19.97203449],
-                                              [-3.01654362, 19.96260676]])
+        expected_ut_data = factor * expected_ut_data
+        expected_vt_data = factor * expected_vt_data
         # Compare u and v data values against previously calculated values.
         self.assertArrayAlmostEqual(ut.data, expected_ut_data)
         self.assertArrayAlmostEqual(vt.data, expected_vt_data)
